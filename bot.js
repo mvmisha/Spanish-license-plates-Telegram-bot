d3dcc2b6957b59aeb7d2275cee7e1a8a8696f480
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2019-03-06 14:58:08 +0100

    First Upload
    Only newer license plates - need to update bot.js

---
+++ bot.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('./config.json')
+const token = config.token;
+const patt = new RegExp('[0-9]{4}[a-zA-Z]{3}');
+const bot = new TelegramBot(token, {
+	polling: true
+});
+
+var Datos;
+bot.on('message', (msg) => {
+	const chatId = msg.chat.id;
+	var matricula = msg.text;
+	if (patt.test(matricula)) {
+		bot.sendMessage(chatId, 'Es una matricula valida, dame un segundo y te mando los datos');
+		(async () => {
+			const browser = await puppeteer.launch();
+			const page = await browser.newPage();
+			await page.goto('http://www.atib.es/TA/modelos/Modelo.aspx?m=621');
+			await page.type('#cph_main_cph_main_621_0_nifC', 'ABC');
+			await page.type('#cph_main_cph_main_621_0_apenomC', 'ABC');
+			await page.type('#cph_main_cph_main_621_0_numMatricu2', matricula);
+			await page.type('#cph_main_cph_main_621_0_txtnif', 'ABC');
+			await page.type('#cph_main_cph_main_621_0_txtnombre', 'ABC');
+			await page.click('#cph_main_cph_main_621_0_lnkBuscarTrafico');
+			await page.waitFor(7600);
+			Datos = await page.evaluate(() => {
+				return {
+					bastidor: document.getElementById('cph_main_cph_main_621_0_bastidorSV').value,
+					marca: document.getElementById('cph_main_cph_main_621_0_marcaVeh').value,
+					modelo: document.getElementById('cph_main_cph_main_621_0_modeloVeh').value,
+					cilindrada: document.getElementById('cph_main_cph_main_621_0_cilindrada').value,
+					fechaMat: document.getElementById('cph_main_cph_main_621_0_fechaMatri').value
+				};
+			});
+			console.log('Datos:', Datos);
+			bot.sendMessage(chatId, 'Esto es lo que he encontrado:\n Marca: ' + Datos.marca + '\nModelo: ' + Datos.modelo + '\nBastidor: ' + Datos.bastidor + '\nCilindrada: ' + Datos.cilindrada + '\nFecha de matriculacion: ' + Datos.fechaMat);
+		})();
+		
+
+	} else {
+		bot.sendMessage(chatId, 'No es una matricula valida');
+	}
+});
+
