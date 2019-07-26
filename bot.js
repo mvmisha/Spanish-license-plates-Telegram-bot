16896966adffbc51e29bf720046aead89b7f0fe3
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2019-07-26 14:49:25 +0200

    waitforselector

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ bot.on('message', (msg) => {
             await page.type('#cph_main_cph_main_621_0_txtnif', matricula);
             await page.type('#cph_main_cph_main_621_0_txtnombre', matricula);
             await page.click('#cph_main_cph_main_621_0_lnkBuscarTrafico');
-            await page.waitFor(20000);
+            await page.waitForSelector('#cph_main_cph_main_621_0_bastidorSV');
             Datos = await page.evaluate(() => {
                 return {
                     bastidor: document.getElementById('cph_main_cph_main_621_0_bastidorSV').value,
