9c4030ddaf033618d9db3b73e4efecef3cf493fa
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2025-03-11 18:36:14 +0100

    delete all

--- README.md
+++
@@ -1,7 +0,0 @@
-# License Plates Bot
-
-A Telegram Bot build with Clojure that search for Spanish license plates information
-
-## Usage
-
-TODO
--- test/licenseplatesbot/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns licenseplatesbot.core-test
-  (:require [clojure.test :refer :all]
-            [licenseplatesbot.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
